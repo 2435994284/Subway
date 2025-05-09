5f897ac7e00c1793296076d1b05b7dd25217b83b
Jiancheng Lian <2435994284@qq.com>
2025-05-09 21:32:36 +0800

    2

--- src/Line.java
+++ src/Line.java
@@ -1,3 +1,16 @@
+import java.util.*;
 public class Line {
+    private String name;
+    private List<station> stations;
+    public Line(String name) {
+        this.name = name;
+        this.stations = new ArrayList<>();
+    }
+    public void addStation(station station) {
+        stations.add(station);
+    }
+    public List<station>getStations(){
+        return stations;
+    }
     
 }
