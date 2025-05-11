27ee1f51ffad3fd44de6cf6024dd6ed1841d0513
Jiancheng Lian <2435994284@qq.com>
2025-05-11 22:47:08 +0800

    4

--- src/SubwayNetwork.java
+++ src/SubwayNetwork.java
@@ -14,8 +14,36 @@ public class SubwayNetwork {
     public void loadData(String filePath) throws Exception{
         BufferedReader br = new BufferedReader(new FileReader(filePath));
         String line;
-        while ((line= =br.readLine()) !=null) {
-            String[] parts= line.split(",");
+        String currentLineName =null;
+        while ((line= br.readLine()) !=null) {
+            line =line.trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            if(line.matches(".*号线.*")){
+                currentLineName = line.split("站点间距")[0].trim();
+                lines.putIfAbsent(currentLineName, new Line(currentLineName));
+            }
+            else if(line.contains("---")||lines.contains("-")){
+                line = line.replace("-","---");
+                String[] parts = line.split("---");
+                if(parts.length !=2){
+                    continue;
+                }
+                String stationA = parts[0].trim();
+                String [] stationBAndDistance =parts[1].trim().split("\\s+");
+                String stationB = stationBAndDistance[0].trim();
+                double distance = Double.parseDouble(stationBAndDistance[1].trim());
+
+                stations.putIfAbsent(stationA, new station(stationA));
+                stations.putIfAbsent(stationB, new station(stationB));
+
+                 if (currentLineName != null) {
+                    lines.get(currentLineName).addStation(stations.get(stationA));
+                    lines.get(currentLineName).addStation(stations.get(stationB));
+                }
+
+            }
             
         }
     }
